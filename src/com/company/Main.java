663da12179866f549c42a635545cfbeae672b2be
alesya.kolbik <moyaalesia@mail.ru>
2017-11-01 18:01:03 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+import  java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число:");
+        int N = in.nextInt();
+        for (int i = 1; i < N; i++) {
+            if (i < 10) {
+                System.out.print(i + " ");
+            }
+            if (i >= 10 && i < 100) {
+                int a = i % 10;
+                int b = (i - a) / 10;
+                if (a == b) {
+                    System.out.print(i + " ");
+                }
+            }
+            if (i >= 100 && i < 1000) {
+                int c = i % 10;
+                int e = i % 100;
+                int d = (i - e) / 100;
+                if (c == d) {
+                    System.out.print(i + " ");
+                }
+            }
+            if (i >= 1000 && i < 10000) {
+                int f = i % 10;
+                int f2 = ((i - f) / 10) % 10;
+                int f3 = ((i - f - (i - f) / 10)/100)% 10;
+                int f4 = (i - f - f2 * 10 - f3 * 100) / 1000;
+                if (f == f4 && f2 == f3) {
+                    System.out.print(i + " ");
+                }
+            }
+            if (i >= 10000 && i < 100000) {
+                int n = i % 10;
+                int n2 = ((i - n) / 10) % 10;
+                int n3 = ((i - n - n2 * 10) / 100) % 100;
+                int n4 = ((i - n - n2 * 10 - n3 * 100) / 1000) % 1000;
+                int n5 = (i - n - n2 * 10 - n3 * 100 - n4 * 1000) / 10000;
+                if (n == n5 && n2 == n4) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+}
